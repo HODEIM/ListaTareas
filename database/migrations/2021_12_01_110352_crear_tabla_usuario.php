c370aab124d377984b4a638bbf378758fb6784c6
hodei <hmaidaganur20dw@ikzubirimanteo.com>
2021-12-01 12:25:51 +0100

    He creado las dos nuevas migraciones para la base de datos y terminado lo de los modelos tambien

--- database/migrations/2021_12_01_110352_crear_tabla_usuario.php
+++ database/migrations/2021_12_01_110352_crear_tabla_usuario.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CrearTablaLista extends Migration
+class CrearTablaUsuario extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CrearTablaLista extends Migration
      */
     public function up()
     {
-        Schema::create('tareas', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre');
-            $table->timestamps();
-        });
+     Schema::create('usuarios', function (Blueprint $table) {
+        $table->id();
+        $table->string("nombre");
+        $table->string("apellidos");
+        $table->timestamps();
+     });
     }
 
     /**
@@ -27,6 +28,6 @@ class CrearTablaLista extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists("tareas");
+        Schema::dropIfExists("usuarios");
     }
 }
